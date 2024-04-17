077e4dbc666860e892f348c6cb3d95293a36e7b6
Jyothika-S <jyothi0351@gmail.com>
2024-04-17 18:19:17 +0530

    fill data moved to testData

--- src/tests/register.spec.ts
+++ src/tests/register.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { RegisterPage } from '../pages/registerPage';
+import demoqaTestData from '../test-data/demoqaTestData.json'
 
 test('Register page', async ({ page }) => {
-    test.setTimeout(40000);
     const register = new RegisterPage(page);
     await register.gotoRegisterPage()
-    await register.register('tom', 'smith', 'tom', 'Tomsmith12!' )
+    await register.register(demoqaTestData.fillValues.firstname, demoqaTestData.fillValues.lastname, demoqaTestData.fillValues.username, demoqaTestData.fillValues.password)
 });
